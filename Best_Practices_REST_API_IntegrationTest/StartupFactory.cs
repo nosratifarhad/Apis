c92003dfb4b8c4a51397d9aeb923db57b9bb715f
f_nosrati <f_nosrati@SHL-62>
2023-03-18 18:45:06 +0330

    Add Integration test

---
+++ Best_Practices_REST_API_IntegrationTest/StartupFactory.cs
@@ -0,0 +1,10 @@
+﻿using BestPracticesRESTAPI;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Best_Practices_REST_API_IntegrationTest
+{
+    public class StartupFactory : WebApplicationFactory<Startup>
+    {
+
+    }
+}
